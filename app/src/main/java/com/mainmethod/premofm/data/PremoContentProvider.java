ed9265791dc20bfa7f55929fdac09bb0ade47b60
Evan Halley <evan.halley@gmail.com>
2016-06-17 11:50:10 -0400

    added some logging

--- app/src/main/java/com/mainmethod/premofm/data/PremoContentProvider.java
+++ app/src/main/java/com/mainmethod/premofm/data/PremoContentProvider.java
@@ -275,6 +275,7 @@ public class PremoContentProvider extends ContentProvider {
 
         switch (match) {
             case EPISODES:
+                Timber.d("Updating episode %s", selectionArgs[0]);
                 values.put(PremoContract.EpisodeEntry.UPDATED_AT, DatetimeHelper.getTimestamp());
                 rowsUpdated = mOpenHelper.getWritableDatabase().update(
                         PremoContract.EpisodeEntry.TABLE_NAME, values, selection, selectionArgs);
