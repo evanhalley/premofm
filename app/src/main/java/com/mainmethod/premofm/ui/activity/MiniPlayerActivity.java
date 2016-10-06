84b12393ad79e64d7452d26e24c02f19a89a9052
Evan Halley <evan@Evans-MacBook-Pro.local>
2016-10-06 14:15:30 -0400

    added time remaining to the mini player

--- app/src/main/java/com/mainmethod/premofm/ui/activity/MiniPlayerActivity.java
+++ app/src/main/java/com/mainmethod/premofm/ui/activity/MiniPlayerActivity.java
@@ -5,12 +5,24 @@
 
 package com.mainmethod.premofm.ui.activity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.media.session.PlaybackState;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
 
 import com.mainmethod.premofm.R;
+import com.mainmethod.premofm.helper.BroadcastHelper;
+import com.mainmethod.premofm.helper.DatetimeHelper;
 import com.mainmethod.premofm.object.Episode;
 import com.mainmethod.premofm.ui.view.MiniPlayer;
+
+import java.lang.ref.WeakReference;
+import java.util.Date;
+
+import timber.log.Timber;
 
 /**
  * An activity that has a mini player
@@ -21,10 +33,25 @@ public abstract class MiniPlayerActivity
 
     // mini player views
     private MiniPlayer mMiniPlayer;
+    private ProgressUpdateReceiver mProgressUpdateReceiver;
 
     @Override
     protected void onCreateBase(Bundle savedInstanceState) {
         setupMiniPlayer();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mProgressUpdateReceiver = new ProgressUpdateReceiver(this);
+        LocalBroadcastManager.getInstance(this).registerReceiver(mProgressUpdateReceiver,
+                new IntentFilter(BroadcastHelper.INTENT_PROGRESS_UPDATE));
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mProgressUpdateReceiver);
     }
 
     @Override
@@ -46,5 +73,33 @@ public abstract class MiniPlayerActivity
      */
     private void setupMiniPlayer() {
         mMiniPlayer = new MiniPlayer(findViewById(R.id.mini_player), this);
+    }
+
+    private static class ProgressUpdateReceiver extends BroadcastReceiver {
+        private static final long UPDATE_INTERVAL_MS = 15_000;
+        private final WeakReference<MiniPlayerActivity> mActivity;
+        private long mLastUpdate = -1;
+
+        public ProgressUpdateReceiver(MiniPlayerActivity context) {
+            mActivity = new WeakReference<>(context);
+        }
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            MiniPlayerActivity activity = mActivity.get();
+
+            if (activity == null) {
+                return;
+            }
+            long timestamp = DatetimeHelper.getTimestamp();
+
+            if (mLastUpdate == -1 || (timestamp - mLastUpdate) > UPDATE_INTERVAL_MS) {
+                Timber.d("Updating mini player time left");
+                mLastUpdate = timestamp;
+                mActivity.get().mMiniPlayer.setDuration(
+                        intent.getLongExtra(BroadcastHelper.EXTRA_DURATION, 0L),
+                        intent.getLongExtra(BroadcastHelper.EXTRA_PROGRESS, 0L));
+            }
+        }
     }
 }
